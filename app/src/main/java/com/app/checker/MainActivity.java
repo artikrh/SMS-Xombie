f09bbdb460e70b3bb808f14a770db7a41de8afc7
Arti Karahoda <38540757+artikrh@users.noreply.github.com>
2018-11-11 17:44:16 +0100

    Added validation for network connection

--- app/src/main/java/com/app/checker/MainActivity.java
+++ app/src/main/java/com/app/checker/MainActivity.java
@@ -4,10 +4,13 @@ import android.app.AlarmManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     @Override
@@ -20,16 +23,42 @@ public class MainActivity extends AppCompatActivity {
         btnFetch.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent i= new Intent(getApplicationContext(), Fetcher.class);
-                startService(i);
+                if(isConnected()){
+                    Intent i= new Intent(getApplicationContext(), Fetcher.class);
+                    startService(i);
+                } else {
+                    Toast toast = Toast.makeText(getApplicationContext(), "Not connected", Toast.LENGTH_SHORT);
+                    toast.show();
+                }
             }
         });
 
-        Intent invokeService = new Intent(this, Fetcher.class);
-        PendingIntent pintent = PendingIntent.getService(this, 0, invokeService, 0);
-        AlarmManager alarm = (AlarmManager)getSystemService(Context.ALARM_SERVICE);
-        //alarm.cancel(pintent);
-        alarm.setRepeating(AlarmManager.RTC_WAKEUP, System.currentTimeMillis(),60000, pintent);
+
+        if(isConnected()){
+            Intent invokeService = new Intent(this, Fetcher.class);
+            PendingIntent pintent = PendingIntent.getService(this, 0, invokeService, 0);
+            AlarmManager alarm = (AlarmManager)getSystemService(Context.ALARM_SERVICE);
+            alarm.setRepeating(AlarmManager.RTC_WAKEUP, System.currentTimeMillis(),60000, pintent);
+        } else {
+            Toast toast = Toast.makeText(getApplicationContext(), "Not connected", Toast.LENGTH_SHORT);
+            toast.show();
+        }
+
 
     }
+
+    public  boolean isConnected()
+    {
+        boolean connected;
+        ConnectivityManager connectivityManager = (ConnectivityManager)getSystemService(Context.CONNECTIVITY_SERVICE);
+        if(connectivityManager.getNetworkInfo(ConnectivityManager.TYPE_MOBILE).getState() == NetworkInfo.State.CONNECTED ||
+                connectivityManager.getNetworkInfo(ConnectivityManager.TYPE_WIFI).getState() == NetworkInfo.State.CONNECTED) {
+            connected = true;
+        }
+        else
+            connected = false;
+
+        return connected;
+    }
+
 }
